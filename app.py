149a3794f9f5644e6d33c5620ec4070f219af2d1
rutujak28 <rutujakantak28@gmail.com>
2026-06-04 19:00:49 +0530

    Finalize PageSense AI UI and RAG improvements

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from rag import create_vector_store
 import streamlit as st
 from scraper import get_page_text
 from ai_engine import ask_gemini
+import time
 
 # --------------------------------------------------
 # PAGE CONFIG
@@ -115,7 +116,7 @@ Transform Any Webpage Into Study Material
 </h3>
 
 <p style='color:#64748B;font-size:18px;'>
-Generate Notes • Viva Answers • Exam Solutions • Assignments
+Learn, summarize and study any webpage using AI
 </p>
 
 </div>
@@ -156,220 +157,212 @@ if st.button(" Analyze Webpage"):
 
     else:
         try:
-            with st.spinner("Processing webpage..."):
+            with st.spinner("Creating knowledge base..."):
 
                 content = get_page_text(url)
-
+                
                 index, chunks = create_vector_store(content)
-
+                
                 st.session_state.index = index
                 st.session_state.chunks = chunks
                 st.session_state.processed_url = url
 
-            st.success(" Webpage Processed Successfully")
-
-            st.info(f"""
-             Chunks Created: {len(chunks)}
-
-             Source Loaded
-
-             Ready for Questions
-            """)
 
         except Exception as e:
-            st.error(f"Error: {e}")
+               st.error(f"Error: {e}")
 
+if st.session_state.index is not None:
 # --------------------------------------------------
 # QUESTION + MODE
 # --------------------------------------------------
 
-st.markdown("---")
+        st.markdown("---")
 
-left, right = st.columns([3, 1])
+        left, right = st.columns([3, 1])
 
-with left:
+        with left:
 
-    st.markdown("###  Ask a Question")
+            st.markdown("###  Ask a Question")
 
-    question = st.text_input(
-        "",
-        placeholder="Ask anything about the webpage..."
-    )
+            question = st.text_input(
+                "",
+                placeholder="Ask anything about the webpage..."
+            )
 
-with right:
+        with right:
 
 
-    mode = st.radio(
-         " Study Mode",
-    [
-        "Normal",
-        "Viva",
-        "Exam",
-        "Assignment",
-        "Notes"
-    ],
-    horizontal=True
-    )
+            mode = st.radio(
+                " Study Mode",
+            [
+                "Normal",
+                "Viva",
+                "Exam",
+                "Assignment",
+                "Notes"
+            ],
+            horizontal=True
+            )
 
-# --------------------------------------------------
-# WORD LIMIT
-# --------------------------------------------------
+        # --------------------------------------------------
+        # WORD LIMIT
+        # --------------------------------------------------
 
-word_limit = None
+        word_limit = None
 
-if mode == "Assignment":
+        if mode == "Assignment":
 
-    word_limit = st.number_input(
-        "Word Limit",
-        min_value=50,
-        max_value=2000,
-        value=400
-    )
+            word_limit = st.number_input(
+                "Word Limit",
+                min_value=50,
+                max_value=2000,
+                value=400
+            )
 
-# --------------------------------------------------
-# QUICK ACTIONS
-# --------------------------------------------------
+        # --------------------------------------------------
+        # QUICK ACTIONS
+        # --------------------------------------------------
 
-st.markdown("###  Quick Actions")
+        st.markdown("###  Quick Actions")
 
-col1, col2, col3 = st.columns(3)
+        col1, col2, col3 = st.columns(3)
 
-with col1:
-    summarize = st.button(" Summary")
+        with col1:
+            summarize = st.button(" Summary")
 
-with col2:
-    keypoints = st.button(" Key Points")
+        with col2:
+            keypoints = st.button(" Key Points")
 
-with col3:
-    exam_topics = st.button(" Exam Topics")
-if exam_topics:
+        with col3:
+            exam_topics = st.button(" Exam Topics")
+        if exam_topics:
 
-      if st.session_state.index is None:
-        st.error("Please click Analyze Webpage first.")
+            if st.session_state.index is None:
+                st.error("Please click Analyze Webpage first.")
 
-      else:
-        try:
-
-            with st.spinner("Generating exam topics..."):
-
-                answer = ask_gemini(
-                    "Generate important exam topics from this webpage.",
-                    st.session_state.index,
-                    st.session_state.chunks,
-                    mode,
-                    word_limit
-                )
-
-            st.markdown("##  Important Exam Topics")
-            st.write(answer)
-
-        except Exception as e:
-            st.error(f"Error: {e}")
-
-# --------------------------------------------------
-# SUMMARIZE
-# --------------------------------------------------
-
-if summarize:
-
-    if st.session_state.index is None:
-        st.error("Please click Analyze Webpage first.")
-
-    else:
-        try:
-
-            with st.spinner("Generating summary..."):
-
-                answer = ask_gemini(
-                    "Summarize this webpage.",
-                    st.session_state.index,
-                    st.session_state.chunks,
-                    mode,
-                    word_limit
-                )
-
-            st.markdown("##  Summary")
-            st.write(answer)
-
-        except Exception as e:
-
-            if "429" in str(e):
-                st.error(
-                    " Gemini API limit reached. Please try again later."
-                )
             else:
-                st.error(f"Error: {e}")
+                try:
 
-# --------------------------------------------------
-# KEY POINTS
-# --------------------------------------------------
+                    with st.spinner("Generating exam topics..."):
 
-if keypoints:
+                        answer = ask_gemini(
+                            "Generate important exam topics from this webpage.",
+                            st.session_state.index,
+                            st.session_state.chunks,
+                            mode,
+                            word_limit
+                        )
 
-    if st.session_state.index is None:
-        st.error("Please click Analyze Webpage first.")
+                    st.markdown("##  Important Exam Topics")
+                    st.write(answer)
 
-    else:
-        try:
+                except Exception as e:
+                    st.error(f"Error: {e}")
 
-            with st.spinner("Finding key points..."):
+        # --------------------------------------------------
+        # SUMMARIZE
+        # --------------------------------------------------
 
-                answer = ask_gemini(
-                    "Give the 5 most important points from this webpage.",
-                    st.session_state.index,
-                    st.session_state.chunks,
-                    mode,
-                    word_limit
-                )
+        if summarize:
 
-            st.markdown("## Key Points")
-            st.write(answer)
+            if st.session_state.index is None:
+                st.error("Please click Analyze Webpage first.")
 
-        except Exception as e:
-
-            if "429" in str(e):
-                st.error(
-                    "Gemini API limit reached. Please try again later."
-                )
             else:
-                st.error(f"Error: {e}")
+                try:
 
-# --------------------------------------------------
-# ASK AI
-# --------------------------------------------------
+                    with st.spinner("Generating summary..."):
 
-if st.button(" Ask PageSense AI"):
+                        answer = ask_gemini(
+                            "Summarize this webpage.",
+                            st.session_state.index,
+                            st.session_state.chunks,
+                            mode,
+                            word_limit
+                        )
 
-    if not url:
-        st.error("Please enter a URL first.")
+                    st.markdown("##  Summary")
+                    st.write(answer)
 
-    elif not question:
-        st.error("Please enter a question.")
+                except Exception as e:
 
-    elif st.session_state.index is None:
-        st.error("Please click Analyze Webpage first.")
+                    if "429" in str(e):
+                        st.error(
+                            " Gemini API limit reached. Please try again later."
+                        )
+                    else:
+                        st.error(f"Error: {e}")
 
-    else:
-        try:
+        # --------------------------------------------------
+        # KEY POINTS
+        # --------------------------------------------------
 
-            with st.spinner("Thinking..."):
+        if keypoints:
 
-                answer = ask_gemini(
-                    question,
-                    st.session_state.index,
-                    st.session_state.chunks,
-                    mode,
-                    word_limit
-                )
+            if st.session_state.index is None:
+                st.error("Please click Analyze Webpage first.")
 
-            st.markdown("## Answer")
-            st.write(answer)
-
-        except Exception as e:
-
-            if "429" in str(e):
-                st.error(
-                    " Gemini API limit reached. Please try again later."
-                )
             else:
-                st.error(f"Error: {e}")
+                try:
+
+                    with st.spinner("Finding key points..."):
+
+                        answer = ask_gemini(
+                            "Give the 5 most important points from this webpage.",
+                            st.session_state.index,
+                            st.session_state.chunks,
+                            mode,
+                            word_limit
+                        )
+
+                    st.markdown("## Key Points")
+                    st.write(answer)
+
+                except Exception as e:
+
+                    if "429" in str(e):
+                        st.error(
+                            "Gemini API limit reached. Please try again later."
+                        )
+                    else:
+                        st.error(f"Error: {e}")
+
+        # --------------------------------------------------
+        # ASK AI
+        # --------------------------------------------------
+
+        if st.button(" Ask PageSense AI"):
+
+            if not url:
+                st.error("Please enter a URL first.")
+
+            elif not question:
+                st.error("Please enter a question.")
+
+            elif st.session_state.index is None:
+                st.error("Please click Analyze Webpage first.")
+
+            else:
+                try:
+
+                    with st.spinner("Thinking..."):
+
+                        answer = ask_gemini(
+                            question,
+                            st.session_state.index,
+                            st.session_state.chunks,
+                            mode,
+                            word_limit
+                        )
+
+                    st.markdown("## Answer")
+                    st.write(answer)
+
+                except Exception as e:
+
+                    if "429" in str(e):
+                        st.error(
+                            " Gemini API limit reached. Please try again later."
+                        )
+                    else:
+                        st.error(f"Error: {e}")
